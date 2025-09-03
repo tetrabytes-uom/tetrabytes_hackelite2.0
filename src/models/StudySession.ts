eddf026b4c14daa7d74a82193428715e30332379
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-09-03 23:05:47 +0530

    feat: create study session model

---
+++ src/models/StudySession.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from 'mongoose';
+import { StudySessionType } from '@/types/StudySessionType';
+
+const studySessionSchema = new Schema<StudySessionType>(
+  {
+    planId: { type: Schema.Types.ObjectId, ref: 'StudyPlan', required: true },
+    day: { type: Number, required: true },
+    date: { type: Date, required: true },
+    tasks: [
+      {
+        title: { type: String, required: true },
+        resources: { type: String },
+        estimatedTime: { type: Number, required: true },
+      },
+    ],
+    status: {
+      type: String,
+      enum: ['pending', 'completed', 'missed'],
+      default: 'pending',
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.StudySession ||
+  mongoose.model<StudySessionType>('StudySession', studySessionSchema);

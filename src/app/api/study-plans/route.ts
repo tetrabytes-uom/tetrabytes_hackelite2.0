fe8175252e58d248320c1fe5e061f5e88ac0664a
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-09-11 15:53:28 +0530

    chore: change the path of authoptions

--- src/app/api/study-plans/route.ts
+++ src/app/api/study-plans/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth';
 import connectDB from '@/lib/mongodb';
 import ManualStudyPlan from '@/models/ManualStudyPlan';
 import User from '@/models/User';
